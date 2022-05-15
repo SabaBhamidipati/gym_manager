fc87d33f5c631d458fa112486516538ec540e1c2
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-05-15 19:19:45 -0400

    feat: created main branch and save changes

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,3 +1,11 @@
 class Gym < ApplicationRecord
+  has_many :members
+
+  def self.order_by_created_at
+    order("created_at desc")
+  end
+
+  def self.total_members_associated_with_gym
+    members.count
+  end
 end
-#push main branch to origin
