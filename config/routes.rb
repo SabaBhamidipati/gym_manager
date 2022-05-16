d48497240a9090980ab7fb944ffa1677b07e7ffc
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-05-16 19:02:55 -0400

    test: passing user story 11

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
   get '/gyms', to: 'gyms#index'
+  get '/gyms/new', to: 'gyms#new'
   get '/gyms/:id', to: 'gyms#show'
+  post '/gyms', to: 'gyms#create'
+  #routes should be ordered as new and then :id, any route with two words should come before one with a dynamic variable
+  #or generally, 
 # :implies that route will vary depending on what' spassed to it
 
   get '/members', to: 'members#index'
